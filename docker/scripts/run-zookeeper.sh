8f8f66f0bafc14b7e04e0c345b5af733bfaf1f5d
Bommineni, Raghu <raghunandan.reddy135@gmail.com>
2024-08-11 01:34:47 -0600

    updating zookeeper to reset/write config file everytime it starts

--- docker/scripts/run-zookeeper.sh
+++ docker/scripts/run-zookeeper.sh
@@ -6,5 +6,26 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Source common functions
 source "$SCRIPT_DIR/common-functions.sh"
 
+# Create the zoo.cfg file
+ZOOKEEPER_CONFIG_DIR="$SCRIPT_DIR/../service-data/zookeeper/configs"
+ZOOKEEPER_CONFIG_FILE="$ZOOKEEPER_CONFIG_DIR/zoo.cfg"
+
+mkdir -p "$ZOOKEEPER_CONFIG_DIR"
+
+cat > "$ZOOKEEPER_CONFIG_FILE" <<EOL
+tickTime=2000
+dataDir=/data
+clientPort=2181
+initLimit=5
+syncLimit=2
+
+# Enable Admin Server
+admin.enableServer=true
+admin.serverPort=8080
+
+# Whitelist four-letter word commands
+4lw.commands.whitelist=srvr,ruok,stat,conf,envi
+EOL
+
 # Restart Zookeeper if it's not running
 restart_service "zookeeper" "$SCRIPT_DIR/../docker-compose/docker-compose-zookeeper.yml" "zookeeper"
