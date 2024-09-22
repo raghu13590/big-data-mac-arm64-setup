0199dab70f2ea7ce9c1b605ab8ca7cae85d2405a
Bommineni, Raghu <raghunandan.reddy135@gmail.com>
2024-09-22 15:10:38 -0600

    adding healthchecks to metastore and hiveserver2, moving logic to add hive, hadoop users to supergroup to Dockerfile,

--- docker/dockerfile/hadoop/entrypoint.sh
+++ docker/dockerfile/hadoop/entrypoint.sh
@@ -7,13 +7,6 @@ export HIVE_HOME=/opt/hive-4.0.0
 export CLASSPATH=$CLASSPATH:$HADOOP_HOME/lib/*:$HIVE_HOME/lib/*
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$HADOOP_MAPRED_HOME/bin:$HADOOP_MAPRED_HOME/sbin:$HIVE_HOME/bin
 
-# Add hive and hadoop users to supergroup for full HDFS permissions
-add_users_to_supergroup() {
-    groupadd supergroup
-    usermod -aG supergroup hadoop
-    usermod -aG supergroup hive
-}
-
 # Function to initialize HDFS directories and set permissions
 initialize_hdfs_directories() {
     # Wait for HDFS to be ready (Namenode and Datanode must be started)
@@ -22,20 +15,25 @@ initialize_hdfs_directories() {
         sleep 5
     done
 
-    # Create warehouse directory if it does not exist
-    if ! hdfs dfs -test -d /user/hive/warehouse; then
-        echo "Creating /user/hive/warehouse directory in HDFS..."
-        hdfs dfs -mkdir -p /user/hive/warehouse
-        hdfs dfs -chmod -R 777 /user/hive/warehouse
-        hdfs dfs -chown -R hive:hadoop /user/hive/warehouse
+   # Create /user/hive/warehouse directory if it does not exist
+       if ! hdfs dfs -test -d /user/hive/warehouse; then
+           echo "Creating /user/hive/warehouse directory in HDFS..."
+           hdfs dfs -mkdir -p /user/hive/warehouse
+           hdfs dfs -chmod -R 777 /user/hive/warehouse
+           hdfs dfs -chown -R hive:hadoop /user/hive/warehouse
+       else
+           echo "/user/hive/warehouse directory already exists"
+       fi
 
-        hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging
-        hdfs dfs -chmod -R 777 /tmp
-        hdfs dfs -chmod -R 777 /tmp/hadoop-yarn/staging
-        hdfs dfs -chown -R hive:hadoop /tmp/hadoop-yarn/staging
-    else
-        echo "/user/hive/warehouse directory already exists"
-    fi
+   # Create /tmp/hadoop-yarn/staging directory if it does not exist
+       if ! hdfs dfs -test -d /tmp/hadoop-yarn/staging; then
+           echo "Creating /tmp/hadoop-yarn/staging directory in HDFS..."
+           hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging
+           hdfs dfs -chmod -R 777 /tmp/hadoop-yarn/staging
+           hdfs dfs -chown -R hive:hadoop /tmp/hadoop-yarn/staging
+       else
+           echo "/tmp/hadoop-yarn/staging directory already exists"
+       fi
 }
 
 # Function to initialize Hive schema
@@ -47,9 +45,6 @@ initialize_hive_schema() {
         echo "Hive schema already initialized."
     fi
 }
-
-# Add users to supergroup once at the beginning
-add_users_to_supergroup
 
 # Start the specified Hadoop component
 case "$1" in
@@ -65,15 +60,14 @@ case "$1" in
   nodemanager)
     yarn --config $HADOOP_HOME/etc/hadoop nodemanager
     ;;
-  hiveserver2)
-    # Initialize HDFS directories before starting HiveServer2
-    initialize_hdfs_directories
-    $HIVE_HOME/bin/hiveserver2
-    ;;
   metastore)
     initialize_hdfs_directories
     initialize_hive_schema
     $HIVE_HOME/bin/hive --service metastore
+    ;;
+  hiveserver2)
+    initialize_hdfs_directories
+    $HIVE_HOME/bin/hiveserver2
     ;;
   *)
     exec "$@"
