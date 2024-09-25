a1d9113dcb6a6598882ec275630a332b177164c4
Bommineni, Raghu <raghunandan.reddy135@gmail.com>
2024-09-25 00:57:22 -0600

    updating akhq to use configs and app-data folder

--- docker/scripts/run-akhq.sh
+++ docker/scripts/run-akhq.sh
@@ -6,25 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Source common functions
 source "$SCRIPT_DIR/common-functions.sh"
 
-# Define the AKHQ configuration directory and file
-AKHQ_CONFIG_DIR="$SCRIPT_DIR/../service-data/akhq/configs"
-AKHQ_CONFIG_FILE="$AKHQ_CONFIG_DIR/application.yml"
-
-# Ensure the configuration directory exists
-mkdir -p "$AKHQ_CONFIG_DIR"
-
-# Create the application.yml file with required configurations
-cat > "$AKHQ_CONFIG_FILE" <<EOL
-micronaut:
-  server:
-    port: 9093
-akhq:
-  connections:
-    docker-kafka-server:
-      properties:
-        bootstrap.servers: "kafka:9092"
-EOL
-
 # Start Kafka
 "$SCRIPT_DIR/run-kafka.sh"
 
