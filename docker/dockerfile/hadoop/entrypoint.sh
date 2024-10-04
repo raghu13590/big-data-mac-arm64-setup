b88e50ade3bedd25517a6b6db08ccbbbe666d10e
Bommineni, Raghu <raghunandan.reddy135@gmail.com>
2024-10-04 15:29:10 -0600

    fixing hive version in hadoop cluster to 4.0.1

--- docker/dockerfile/hadoop/entrypoint.sh
+++ docker/dockerfile/hadoop/entrypoint.sh
@@ -3,7 +3,7 @@
 # Set up environment variables
 export HADOOP_HOME=/opt/hadoop-3.3.6
 export HADOOP_MAPRED_HOME=/opt/hadoop-3.3.6
-export HIVE_HOME=/opt/hive-4.0.0
+export HIVE_HOME=/opt/hive-4.0.1
 export CLASSPATH=$CLASSPATH:$HADOOP_HOME/lib/*:$HIVE_HOME/lib/*
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$HADOOP_MAPRED_HOME/bin:$HADOOP_MAPRED_HOME/sbin:$HIVE_HOME/bin
 
