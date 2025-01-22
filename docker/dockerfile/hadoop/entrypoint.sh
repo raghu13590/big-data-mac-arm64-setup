e8ae49561b085be86cfd7502ca1b354a074e6476
Bommineni, Raghu <raghunandan.reddy135@gmail.com>
2025-01-22 14:04:42 -0700

    refactoring hadoop Dockerfile, removing redundant path setup

--- docker/dockerfile/hadoop/entrypoint.sh
+++ docker/dockerfile/hadoop/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Set up environment variables
-export HADOOP_HOME=/opt/hadoop-3.3.6
-export HADOOP_MAPRED_HOME=/opt/hadoop-3.3.6
-export HIVE_HOME=/opt/hive-4.0.1
-export CLASSPATH=$CLASSPATH:$HADOOP_HOME/lib/*:$HIVE_HOME/lib/*
-export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$HADOOP_MAPRED_HOME/bin:$HADOOP_MAPRED_HOME/sbin:$HIVE_HOME/bin
-
 # Function to initialize HDFS directories and set permissions
 initialize_hdfs_directories() {
     # Wait for HDFS to be ready (Namenode and Datanode must be started)
