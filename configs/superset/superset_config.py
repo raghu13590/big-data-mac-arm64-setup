f7fb8d1ca770c5a0c8e21209ed9694e50cee39a7
raghu.bommineni <raghu.bommineni@travelport.com>
2025-10-06 22:40:10 -0500

    moving hardcoded paths to .env file

---
+++ configs/superset/superset_config.py
@@ -0,0 +1,9 @@
+# Superset configuration file
+SECRET_KEY = 'lLRTHJRlvUd4pquTyaDCLsd7oqRqs7yCi297m4QEyI7Y+rowyDgzmuV/'
+ROW_LIMIT = 5000
+SUPERSET_WEBSERVER_PORT = 9088
+
+# Database configuration
+SQLALCHEMY_DATABASE_URI = 'postgresql://superset:superset@superset_db:5432/superset'
+
+# Additional configurations can be added here
